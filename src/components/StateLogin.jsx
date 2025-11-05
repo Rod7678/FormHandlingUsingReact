706c5fb11a5c1f71ba90197670f991435bada848
Rohit <rohitghadge348@gmail.com>
2025-11-06 00:01:21 +0530

    Added email validation With better UX

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,23 +8,40 @@ export default function Login() {
   email: '', 
   password: ''
 });
+ const [ didEdit, setDidEdit] = useState({
+  email: false, 
+  password: false
+});
 
-const emailIsValid = enteredValues.email !== '' && !enteredValues.email.includes('@');
+const emailIsValid =  didEdit.email && !enteredValues.email.includes('@');
 
   function handleInputChanges(identifier, value){
     setEnteredValues((prevValues)=>({
       ...prevValues,
       [identifier] : value
-    }))
+    }));
+
+    setDidEdit((prevEdit)=>({
+      ...prevEdit,
+      [identifier]: false
+    }));
   }
 
 
+  
+  function handleInputBlur(identifier){
+    setDidEdit((prevValues)=>({
+      ...prevValues,
+      [identifier]: true
+    }));
+    
+  }
+  
   function handleSubmit(event){
     event.preventDefault();
     console.log('Submitted!');
     console.log(enteredValues)
   }
-
   // function handleEmailChange(event){
   //   setEnteredEmail(event.target.value)
   // }
@@ -40,6 +57,7 @@ const emailIsValid = enteredValues.email !== '' && !enteredValues.email.includes
           id="email" 
           type="email" 
           name="email" 
+          onBlur={()=> handleInputBlur('email')}
           onChange={(event)=>handleInputChanges('email', event.target.value)} 
           value={enteredValues.email}
           />
