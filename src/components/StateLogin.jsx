38a8653bd8410fff9d640acfc27c8265293b895b
Rohit <rohitghadge348@gmail.com>
2025-11-05 23:01:37 +0530

    State approach

---
+++ src/components/StateLogin.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+ const [ enteredValues, setEnteredValues] = useState({
+  email: '', 
+  password: ''
+});
+
+  function handleInputChanges(identifier, value){
+    setEnteredValues((prevValues)=>({
+      ...prevValues,
+      [identifier] : value
+    }))
+  }
+
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log('Submitted!');
+    console.log(enteredValues)
+  }
+
+  // function handleEmailChange(event){
+  //   setEnteredEmail(event.target.value)
+  // }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input 
+          id="email" 
+          type="email" 
+          name="email" 
+          onChange={(event)=>handleInputChanges('email', event.target.value)} 
+          value={enteredValues.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input 
+          id="password" 
+          type="password" 
+          name="password"  
+          onChange={(event)=>handleInputChanges('password', event.target.value)} 
+          value={enteredValues.password}/>
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
