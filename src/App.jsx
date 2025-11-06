44059d682b555f1fc0dd3ca38a97c4c589e9e06b
Rohit <rohitghadge348@gmail.com>
2025-11-06 22:59:38 +0530

    Added required in signup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
-// import SignUp from './components/SignUp.jsx';
+// import Login from './components/Login.jsx';
+import SignUp from './components/SignUp.jsx';
 // import Login from './components/StateLogin.jsx';
 
 function App() {
@@ -8,8 +8,8 @@ function App() {
     <>
       <Header />
       <main>
-        {/* <SignUp /> */}
-        <Login />
+        <SignUp />
+        {/* <Login /> */}
       </main>
     </>
   );
